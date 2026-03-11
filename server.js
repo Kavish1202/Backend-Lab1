a821f61f4442106250b24dfba42e63bed817dd3d
kavish <solanki.kavish12@gmail.com>
2026-03-11 18:20:21 +0100

    Lab3 Comm1

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 require('dotenv').config(); 
 const express = require('express');
 const mongoose = require('mongoose');
-const helmet = require('helmet');
+const helmet = require('helmet'); 
+const session = require('express-session');
+const flash = require('connect-flash');
 
 const taskRoutes = require('./routes/taskRoutes');
 
@@ -10,10 +12,33 @@ const { verifyAPIKey, verifyJWT } = require('./middleware/authMiddleware');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
+app.use(session({
+    secret: 'my-super-secret-session-key',
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(flash());
+
+app.use((req, res, next) => {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    next();
+});
+
 app.use(helmet()); 
 app.disable('x-powered-by');
 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// --- LAB 3 LANDING PAGE ROUTE ---
+app.get('/', (req, res) => {
+    // This tells Express to look for a file named 'index.ejs' in a 'views' folder
+    res.render('index'); 
+});
 
 app.use('/api/tasks', taskRoutes);
 
